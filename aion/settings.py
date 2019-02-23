402cbedf63fef67b19f067646c76b278312c4d9d
Jeff <jeff@jeff.how>
2019-02-23 04:18:30 +0000

    Requirements Update

--- aion/settings.py
+++ aion/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'v+w(2a-rw**_s(0g)%*&9=b!7x@0^*
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
+    'aion.run',
     'aion2-jeff-how.c9users.io', 
     'aionapp.herokuapp.com'
     ]
